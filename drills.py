6064d2664b4489dbbb3f76d1ac25099e359996d6
peastea <53393858+peastea@users.noreply.github.com>
2022-01-30 13:42:20 +0100

    initial

--- drills.py
+++ drills.py
@@ -1,10 +1,28 @@
 from flask import Flask, render_template, jsonify
+import os
+import json
+import glob
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+def load_drills():
+    with open('./static/drills/drills.json') as json_file:
+        drills = json.load(json_file)['drills']
+        logging.info(drills)
+    return drills
+
+def getpath(drills):
+    return [f"{drill['path']}/komplett.png" for drill in drills]
 
 app = Flask(__name__)
 
+
+
 @app.route("/")
 def home():
-    return render_template('/alldrills.html')
+    drills = load_drills()
+    return render_template('/alldrills.html', paths=getpath(drills))
 
 if __name__ == "__main__":
     app.run()
