726dca9d42a254fa95c040fac900b50558b28385
peastea <53393858+peastea@users.noreply.github.com>
2022-03-18 21:54:27 +0100

    added query for single goalie

--- static/js/alldrills.js
+++ static/js/alldrills.js
@@ -1,5 +1,6 @@
 var drills = {};
-    
+var singleGoalie = false;
+
 $( document ).ready(function() {
     console.log( "ready!" );
     $.getJSON('/alldrills',
@@ -17,17 +18,35 @@ $(document).on('input', '#minSlider', function() {
     displayDrills();
 });
 
+$(document).on('change', '#swtGoalies', function() {
+    var test = $('#swtGoalies').prop('checked');
+    displayDrills();
+});
+
 function displayDrills(){
     var html = '';
-    for(i=0; i<drills.length; i++){
-        if (drills[i].minPlayers <= $('#minSlider').val()){
-            html += "<a class='overview-link overview-item' href='/singledrill/"+drills[i].name+"'> \
-            <h5>" + drills[i].name+ "</h5> \
-            <div class='overview-image'> \
-                <img src='/static/"+drills[i].path+"/komplett.png'> \
-            </div>  \
-            </a>";
-        }            
+    if($('#swtGoalies').prop('checked')){
+        for(i=0; i<drills.length; i++){
+            if (drills[i].minPlayers <= $('#minSlider').val() && drills[i].singleGoalie){
+                html += addDrill(drills[i]);
+            }            
+        }
+    } else {
+        for(i=0; i<drills.length; i++){
+            if (drills[i].minPlayers <= $('#minSlider').val()){
+                html += addDrill(drills[i]);
+            }            
+        }
     }
+    
     $('#content').html(html);
+}
+
+function addDrill (drill){
+    return "<a class='overview-link overview-item' href='/singledrill/"+drills[i].name+"'> \
+    <h5>" + drills[i].name+ "</h5> \
+    <div class='overview-image'> \
+        <img src='/static/"+drills[i].path+"/komplett.png'> \
+    </div>  \
+    </a>";
 }
